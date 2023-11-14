bfc27d4fe59635bab6bef554cf72bb71048bf32f
Austin Gray <GrayAustinC@gmail.com>
2023-11-14 02:32:32 -0500

    Updated Typescript

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import useUpdateEffect from "./useUpdateEffect";
 
 const UpdateSymbol = Symbol("update");
 
-type UpdateFunction = () => any;
+type UpdateFunction = () => void;
+type ConstructorType<ClassState> = new (...args: any[]) => ClassState;
+
+/**
+ * Represents a callback function that, when invoked, returns an instance of a class
+ * whose constructor conforms to the specified ClassState.
+ */
+export type ClassStateCallback<Target extends ClassState> = () => ConstructorType<Target> extends abstract new (...args: any[]) => infer R ? R : any;
 
 export class ClassState {
   [UpdateSymbol]: UpdateFunction = () => {};
@@ -14,7 +21,7 @@ export function updateClassState<UsedClassState extends ClassState>(state: UsedC
   return state[UpdateSymbol]();
 }
 
-function useClassState<Target extends new (...args: any[]) => ClassState>(State: Target, args: ConstructorParameters<Target>): () => InstanceType<Target> {
+function useClassState<Target extends ConstructorType<ClassState>>(State: Target, args: ConstructorParameters<Target>): ClassStateCallback<InstanceType<Target>> {
   const [state, setState] = useState(() => new State(args));
   state[UpdateSymbol] = useUpdate();
   useUpdateEffect(() => setState(new State(args)), args);
